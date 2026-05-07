a3c169ecfbce65a98f46f81bdf1c71f892b63449
Kevin.M <kema2501@student.miun.se>
2026-05-07 20:58:19 +0200

    Testa databasanslutning

--- server.js
+++ server.js
@@ -13,11 +13,26 @@ const port = process.env.PORT || 5501;
 app.use(cors());
 app.use(express.json());
 
-
 // Routes
 app.use("/api", authRoutes);
 
-// Starta applikation
-app.listen(port, () => {
-    console.log(`Server running at http:localhost:${port}`)
-});
+const mongoose = require("mongoose");
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            dbName: "DT207G-Labb4"
+        });
+
+        console.log("Ansluten till databas");
+
+        app.listen(PORT, () => {
+            console.log("Server running");
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+startServer();
